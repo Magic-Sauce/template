144a6bde7b91f8f6dadb9cd0b71297220f430f5b
Steven Layne <stevenolayne@gmail.com>
2020-07-30 14:37:02 -0400

    built

--- dist/templates/event/types.d.ts
+++ dist/templates/event/types.d.ts
@@ -1,7 +1,7 @@
 export declare type RenderEventTemplateData = {
     EVENT_URL: string;
     EVENT_DESCRIPTION?: string;
-    EVENT_IMAGE: string;
+    EVENT_IMAGE?: string;
     EVENT_TITLE: string;
     EVENT_SUBTITLE?: string;
     EVENT_START_DATE?: Date;
