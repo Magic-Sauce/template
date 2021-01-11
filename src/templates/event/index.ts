b10c0eaed8639e1b6eb409d9146966d529113638
Steven Layne <stevenolayne@gmail.com>
2021-01-11 07:59:49 -0500

    added more generic templating

--- src/templates/event/index.ts
+++ src/templates/event/index.ts
@@ -1,10 +1,27 @@
-import * as Mustache from 'mustache';
+import * as Mustache from "mustache";
 
-import { EventPartial } from "./types";
+import { EventPartial, ContentPartial } from "./types";
 
-export const renderEventTemplate = ({template, partials}:{template: string, partials: EventPartial}) => {
+export const renderEventTemplate = ({
+  template,
+  partials,
+}: {
+  template: string;
+  partials: EventPartial;
+}) => {
   const output = Mustache.render(template, partials);
   return output;
-}
+};
 
-export {EventPartial}
+export const renderContentTemplate = ({
+  template,
+  partials,
+}: {
+  template: string;
+  partials: ContentPartial;
+}) => {
+  const output = Mustache.render(template, partials);
+  return output;
+};
+
+export { EventPartial, ContentPartial };
