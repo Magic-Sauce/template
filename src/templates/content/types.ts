ef31f54e829f095b85625ccf6195a01f335c503a
Steven Layne <stevenolayne@gmail.com>
2021-04-03 23:33:22 -0400

    updated vars

--- src/templates/content/types.ts
+++ src/templates/content/types.ts
@@ -1,15 +1,15 @@
 export type ContentPartial = {
   CONTENT_URL: string;
   CONTENT_TITLE: string;
-  CONTENT_PAGE_URL?: string;
-  CONTENT_REFERRAL_URL?: string;
-  CONTENT_IMAGE?: string;
-  CONTENT_SUBTITLE?: string;
+  CONTENT_PAGE_URL: string | null;
+  CONTENT_REFERRAL_URL: string | null;
+  CONTENT_IMAGE: string | null;
+  CONTENT_SUBTITLE: string | null;
   CONTENT_START_DATE?: Date;
   CONTENT_TRIGGER_DATE?: Date;
   CONTENT_END_DATE?: Date;
   CONTENT_PASSWORD?: string;
-  USER_FIRST_NAME?: string;
-  USER_LAST_NAME?: string;
-  USER_FULL_NAME?: string;
+  USER_FIRST_NAME: string | null;
+  USER_LAST_NAME: string | null;
+  USER_FULL_NAME: string | null;
 };
